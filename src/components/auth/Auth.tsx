8eceab43c6b51308e67fd1a48d662b6747f383bc
Zubaira <kasumov.zi@zerolab.tech>
2022-08-10 22:44:58 +0300

    chore: Подключил шрифты

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 type Props = {};
 
 const Auth: React.FC = (props: Props) => {
-	return <div className=''>Auth</div>;
+	return (
+		<div className=''>
+			<p>Whereas recognition of the inherent dignity</p>
+			<p>123</p>
+		</div>
+	);
 };
 
 export default Auth;
