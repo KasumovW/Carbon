80d4dcb58ed12eaf75a0efb97ef973589bad7e12
Zubaira <kasumov.zi@zerolab.tech>
2022-08-11 10:35:54 +0300

    fix: Импорт scss,svg,jpg,jpeg

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Auth.module.scss'
 
 type Props = {};
 
 const Auth: React.FC = (props: Props) => {
 	return (
-		<div className=''>
+		<div className={s.container}>
 			<p>Whereas recognition of the inherent dignity</p>
 			<p>123</p>
 		</div>
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="your-custom-published-react-scripts-name-here" />
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+declare module "*.jpeg" {
+    const content: any;
+    export default content;
+}
+declare module "*.scss" {
+    const content: any;
+    export default content;
+}
