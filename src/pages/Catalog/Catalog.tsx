0e6fc4a1b457a56e4b4bdb33f1c901e465f1133b
Zubaira <kasumov.zi@zerolab.tech>
2022-08-25 10:32:48 +0300

    chore: Выбор каталого

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
-import Card from "../../components/Catalog/Card/Card";
-import s from "./Catalog.module.scss";
+import React, { useState } from 'react';
+import Card from '../../components/Catalog/Card/Card';
+import s from './Catalog.module.scss';
 
 type Props = {};
 
 const Catalog: React.FC = (props: Props) => {
-  interface ICatalog {
-    title?: string;
-    id?: number;
-    active?: boolean;
-  }
+	const [count, setCount] = useState(0);
 
-  const [catalog, setCatalog] = useState<ICatalog[]>([
-    { title: "мобильные аксессуары", id: 1, active: true },
-    { title: "тюнинг авто", id: 2, active: false },
-    { title: "багаж", id: 3, active: false },
-    { title: "динозавры", id: 4, active: false },
-    { title: "подарки", id: 5, active: false },
-    { title: "ложки", id: 6, active: false },
-  ]);
+	const [catalog, setCatalog] = useState([
+		'мобильные аксессуары',
+		'тюнинг авто',
+		'багаж',
+		'динозавры',
+		'подарки',
+		'ложки',
+		// { title: 'мобильные аксессуары', id: 1, active: true },
+		// { title: 'тюнинг авто', id: 2, active: false },
+		// { title: 'багаж', id: 3, active: false },
+		// { title: 'динозавры', id: 4, active: false },
+		// { title: 'подарки', id: 5, active: false },
+		// { title: 'ложки', id: 6, active: false },
+	]);
 
 	return (
 		<div className={s.wrapper}>
 			<div className={s.container}>
 				<h2>каталог товаров</h2>
 				<ul>
-					{catalog.map((element) => (
-						<li className={element.active ? s.active : ''} key={element.id}>
-							{element.title}
+					{catalog.map((element, i) => (
+						<li
+							onClick={() => setCount(i)}
+							className={i === count ? s.active : ''}
+							key={element}
+						>
+							{element}
 						</li>
 					))}
 				</ul>
